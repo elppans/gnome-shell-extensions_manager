ffd36b82c8a6ce29d43da6acdc38f1f5e2cf276f
elppans <elppansmk@gmail.com>
2025-03-09 00:46:56 -0300

    Update gnome-shell-extensions_manager.sh

--- gnome-shell-extensions_manager.sh
+++ gnome-shell-extensions_manager.sh
@@ -14,7 +14,7 @@ manage_extension() {
 }
 
 # Lista de extensões a serem gerenciadas
-extensions_list="$(cat/home/arch/.local/share/gnome-shell/extensions.list)"
+extensions_list="$(cat $HOME/.local/share/gnome-shell/extensions.list)"
 
 # Iterar pela lista e verificar/ativar cada extensão
 for extension in "${extensions_list[@]}"; do
